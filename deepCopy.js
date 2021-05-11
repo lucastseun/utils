93ea6fde52ae5e5a3cb26a5ef6601a5918a9d87a
lucastseun <lucastseun@qq.com>
2021-05-11 21:16:43 +0800

    deeCopy

---
+++ deepCopy.js
@@ -0,0 +1,17 @@
+const deepCopy = (obj) => {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
+    let isArray = (obj) => Object.prototype.toString.call(obj) === '[object Array]';
+
+    let copy = isArray(obj) ? [] : {};
+
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            copy[key] = deepCopy(obj[key]);
+        }
+    }
+
+    return copy;
+}
